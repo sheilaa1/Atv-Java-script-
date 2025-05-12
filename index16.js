85b0dd0460c181852acab68c36de7f1195c42e9d
sheilaa1 <sheilaalicemato@gmail.com>
2025-05-12 18:27:35 +0000

    extras celcius p/ Fahrenheit

---
+++ index16.js
@@ -0,0 +1,6 @@
+function celsiusParaFahrenheit(celsius) {
+      return (celsius * 9/5) + 32;
+      }
+      let temperaturaCelsius = 30;
+      let temperaturaFahrenheit = celsiusParaFahrenheit(temperaturaCelsius);
+      console.log(temperaturaCelsius + "°C é igual a " + temperaturaFahrenheit + "°F");
