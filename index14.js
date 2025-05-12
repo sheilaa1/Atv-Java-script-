6009cc2d080f4f3bb694d14eaa92d047cbdf82d8
sheilaa1 <sheilaalicemato@gmail.com>
2025-05-12 18:20:22 +0000

    arrays e objetos . média

---
+++ index14.js
@@ -0,0 +1,8 @@
+let numeros = [100, 120, 130, 140, 150];
+let soma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+  }
+  let media = soma / numeros.length;
+  console.log("A média é: " + media);
+  
