ed56a42d8b98ef40b213d488ff3ed86fdd8d7946
sheilaa1 <sheilaalicemato@gmail.com>
2025-05-12 18:23:36 +0000

    arrays e objetos carro.

---
+++ index15.js
@@ -0,0 +1,8 @@
+let carro = {
+      marca: "Toyota",
+        modelo: "Corolla",
+          ano: 2022
+          };
+          console.log("Marca: " + carro.marca);
+          console.log("Modelo: " + carro.modelo);
+          console.log("Ano: " + carro.ano);
